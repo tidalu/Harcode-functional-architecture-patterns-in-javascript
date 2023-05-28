7503a21a625d399f160c3e689bf7d296c28e1607
Tidalu <jsjsdq65@gmail.com>
2023-05-29 00:02:36 +0200

    Highly generealized functions: Definition Group:

--- app.js
+++ app.js
@@ -50,19 +50,19 @@
 
     joinWithSpace(joinWithSpace('a', 'v'), 'c') // a v c
 
-    joinWithSpace = joinable => jinable.join(' ');
+    // joinWithSpace = joinable => joinable.join(' ');
 
     joinWithSpace([user.firstName, user.lastName]);
 
 
-    const identit = a => a;
+    // const identit = a => a;
     
-    const {Either} = requires('types')
+    // // const {Either} = requires('types')
 
-    const identity = a => a;
-    compose(f, g);
+    // const identity = a => a;
+    // compose(f, g);
 
-    Either.of(2).fold(identity, identity);
+    // Either.of(2).fold(identity, identity);
 
 
     // Highly generalized function.
@@ -87,9 +87,99 @@
 
 // definition of the group
 
-// 
-// 
+//   
+// // Definition. A group is a set G along with some operation · that takes in two elements
+// and outputs another element of our group, such that we satisfy the following properties:
+// • Identity: there is a unique identity element e ∈ G such that for any other g ∈ G, we
+// have e · g = g · e = g.
+// • Inverses: for any g ∈ G, there is a unique g
+// −1
+// such that g · g
+// −1 = g
+// −1
+// g = e.
+// • Associativity: for any three a, b, c ∈ G, a · (b · c) = (a · b) · c.
 // 
 
 
 // favor composable functions
+
+
+
+// shapes // normalize effect types
+
+
+// ---> semi group
+
+let a =1+2+(3+6)
+a = (1+2)+3+6
+// addition is associative
+
+
+ a = 2 * (5 * 2)
+
+// multiplication is also closed and associative
+
+// closed  is when we do an operation if we get the result as the same type as the two arguments this operation si closed
+
+// closed + associative = parallel
+
+a = true && true && false
+a = true || true || false
+
+// // these are also associative 
+
+// [1, 2, 3] ^ [2, 5, 3] ^ [5, 3] // --> this is also associative
+// [1, 2, 3] v [2, 5, 3] v [5, 3] // --> this is also associative
+
+
+
+// <*>
+// > < 
+
+1 > 3 > 7
+
+
+//
+
+const Sum = (x) => 
+    ({
+        x,
+        concat: other =>
+            Sum(x + other.x)
+    });
+const Product = (x) => 
+    ({
+        x,
+        concat: other =>
+            Product(x * other.x)
+    });
+const any = (x) => 
+    ({
+        x,
+        concat: other =>
+            any(x|| other.x)
+    });
+
+    const res = Sum(3).concat(Sum(5)) // SUm(8)
+    const prod = Product(3).concat(Product(5)) // SUm(8)
+    const bool = any(true).concat(any(false));
+    console.log(res);
+    console.log(prod);
+    console.log(bool);
+    // string
+    console.log('hi'.concat('!'));
+
+
+    // Monoid = SemiGroup + Identity
+    // am using product function above 
+    Product.empty = () => Product(1);
+    console.log(Product.empty().concat(Product(10)));
+
+    // lets go for sum. 
+    Sum.empty = () => Sum(0);
+
+    any(true).concat(any(false)); //
+
+    const sum1 = [].map(Sum).reduce((acc, n) => acc.concat(n), Sum.empty()); // if we give valus give an sum, we will just get the sums
+    console.log(sum1); // else it just blows up, if we initializ the reduce we get smth else it shows an error
